502e7b333a6582207df69de25aca8e5e7a9ca97c
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-15 18:09:36 +0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -22,7 +22,7 @@ const genCode = async (n)=>{
         appToken= '82647f43-3f87-402d-88dd-09a90025313f'
         promoId= 'c4480ac7-e178-4973-8061-9ed5b2e17954'
     }
-     else if (n==5){
+     else if (n==4){
          appToken= 'd28721be-fd2d-4b45-869e-9f253b554e50'
          promoId= '43e35910-c168-4634-ad4f-52fd764a843f'
      }
