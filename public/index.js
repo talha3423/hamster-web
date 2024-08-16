9584584ef611ed6a96522ce58a9b8bb735fe9128
Muhammad Talha <talhariaz5425869@gmail.com>
2024-08-16 20:26:08 +0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -26,10 +26,13 @@ const genCode = async (n)=>{
          appToken= 'd28721be-fd2d-4b45-869e-9f253b554e50'
          promoId= '43e35910-c168-4634-ad4f-52fd764a843f'
      }
-     else {
+     else if(n==5){
         appToken= "8d1cc2ad-e097-4b86-90ef-7a27e19fb833"
         promoId= "dc128d28-c45b-411c-98ff-ac7726fbaea4"
      
+     }else {
+         appToken='61308365-9d16-4040-8bb0-2f4a4c69074c'
+          promoId='61308365-9d16-4040-8bb0-2f4a4c69074c'
      }
     const res = await fetch("https://api.gamepromo.io/promo/login-client", {
        "headers": {
